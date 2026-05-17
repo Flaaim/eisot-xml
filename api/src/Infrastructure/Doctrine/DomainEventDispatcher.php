6d6af9af89bee3d4deef1e5950bff01d358dd69f
Flaaim <flaaim@list.ru>
2026-05-17 23:10:16 +1000

    installed Symfony messenger

--- api/src/Infrastructure/Doctrine/DomainEventDispatcher.php
+++ api/src/Infrastructure/Doctrine/DomainEventDispatcher.php
@@ -4,9 +4,43 @@ declare(strict_types=1);
 
 namespace Infrastructure\Doctrine;
 
+use App\SharedDomain\AggregateRoot;
+use Doctrine\ORM\Event\OnFlushEventArgs;
+use Symfony\Component\Messenger\MessageBusInterface;
+
 final class DomainEventDispatcher
 {
-    public function onFlush(): void {}
+    private array $events = [];
 
-    public function postFlush(): void {}
+    public function __construct(
+        private readonly MessageBusInterface $messageBus
+    ) {}
+    public function onFlush(OnFlushEventArgs $args): void
+    {
+        $em = $args->getObjectManager();
+        $uow = $em->getUnitOfWork();
+
+        $entities = [
+            ...$uow->getScheduledEntityInsertions(),
+            ...$uow->getScheduledEntityUpdates(),
+            ...$uow->getScheduledEntityDeletions(),
+        ];
+
+        foreach ($entities as $entity) {
+            if ($entity instanceof AggregateRoot) {
+                foreach ($entity->releaseEvents() as $event) {
+                    $this->events[] = $event;
+                }
+            }
+        }
+    }
+
+    public function postFlush(): void
+    {
+        $events = $this->events;
+        $this->events = [];
+        foreach ($events as $event) {
+            $this->messageBus->dispatch($event);
+        }
+    }
 }
