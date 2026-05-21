167a17bf61e77f97f98464180c198f811a7840dc
Flaaim <flaaim@list.ru>
2026-05-21 22:40:54 +1000

    added ValidationSubscriber

--- api/src/Infrastructure/Http/EventSubscriber/ValidationExceptionSubscriber.php
+++ api/src/Infrastructure/Http/EventSubscriber/ValidationExceptionSubscriber.php
@@ -4,9 +4,13 @@ declare(strict_types=1);
 
 namespace App\Infrastructure\Http\EventSubscriber;
 
+use Infrastructure\Http\Validator\ValidationException;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Validator\ConstraintViolationInterface;
+use Symfony\Component\Validator\ConstraintViolationListInterface;
 
 final class ValidationExceptionSubscriber implements EventSubscriberInterface
 {
@@ -14,15 +18,31 @@ final class ValidationExceptionSubscriber implements EventSubscriberInterface
     public static function getSubscribedEvents(): array
     {
         return [
-            KernelEvents::REQUEST => ['onKernelRequest', 200],
+            KernelEvents::EXCEPTION => ['onKernelException', 10],
         ];
     }
 
-    public function onKernelRequest(RequestEvent $event): void
+    public function onKernelException(ExceptionEvent $event): void
     {
-        if (!$event->isMainRequest()) {
+        $exception = $event->getThrowable();
+
+        if(!$exception instanceof ValidationException){
             return;
         }
-        $request = $event->getRequest();
+
+        $response = new JsonResponse([
+            'errors' => self::errorsArray($exception->getViolations())
+        ], JsonResponse::HTTP_UNPROCESSABLE_ENTITY);
+
+        $event->setResponse($response);
+    }
+    private static function errorsArray(ConstraintViolationListInterface $violations): array
+    {
+        $errors = [];
+        /** @var ConstraintViolationInterface $violation */
+        foreach ($violations as $violation) {
+            $errors[$violation->getPropertyPath()] = $violation->getMessage();
+        }
+        return $errors;
     }
 }
---
+++ api/src/Infrastructure/Http/Test/Subscriber/ValidationExceptionSubscriberTest.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Http\Test\Subscriber;
+
+use DomainException;
+use App\Infrastructure\Http\EventSubscriber\ValidationExceptionSubscriber;
+use Infrastructure\Http\Validator\ValidationException;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Validator\ConstraintViolation;
+use Symfony\Component\Validator\ConstraintViolationList;
+
+final class ValidationExceptionSubscriberTest extends TestCase
+{
+    public function testSubscribedEvents(): void
+    {
+        $subscriber = ValidationExceptionSubscriber::getSubscribedEvents();
+        self::assertArrayHasKey(KernelEvents::EXCEPTION, $subscriber);
+
+        self::assertEquals(['onKernelException', 10], $subscriber[KernelEvents::EXCEPTION]);
+    }
+
+    public function testProcessException(): void
+    {
+        $exception = new DomainException('DomainException');
+        $subscriber = new ValidationExceptionSubscriber();
+        $request = new Request();
+
+        $kernel = $this->createMock(HttpKernelInterface::class);
+        $event = new ExceptionEvent(
+            $kernel,
+            $request,
+            HttpKernelInterface::MAIN_REQUEST,
+            $exception
+        );
+
+        $subscriber->onKernelException($event);
+        $response = $event->getResponse();
+
+        self::assertNull($response);
+    }
+
+    public function testProcessValidationException(): void
+    {
+
+        $violations = new ConstraintViolationList([
+            new ConstraintViolation('Incorrect Email', null, [], null, 'email', 'not-email'),
+            new ConstraintViolation('Empty Password', null, [], null, 'password', ''),
+        ]);
+
+        $exception = new ValidationException($violations);
+
+        $subscriber = new ValidationExceptionSubscriber();
+
+        $request = new Request();
+        $kernel = $this->createMock(HttpKernelInterface::class);
+
+        $event = new ExceptionEvent(
+            $kernel,
+            $request,
+            HttpKernelInterface::MAIN_REQUEST,
+            $exception
+        );
+
+        $subscriber->onKernelException($event);
+        $response = $event->getResponse();
+
+        self::assertNotNull($response);
+
+        self::assertInstanceOf(JsonResponse::class, $response);
+        self::assertEquals(JsonResponse::HTTP_UNPROCESSABLE_ENTITY, $response->getStatusCode());
+
+        self::assertJson($body = $response->getContent());
+        $data = json_decode($body, true, 512, JSON_THROW_ON_ERROR);
+
+        self::assertEquals(['errors' => [
+            'email' => 'Incorrect Email',
+            'password' => 'Empty Password',
+        ]], $data);
+    }
+
+}
