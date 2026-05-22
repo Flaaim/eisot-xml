21058fa4a2565121bcce5bd3e68868bc41ddd1ba
Flaaim <flaaim@list.ru>
2026-05-22 14:33:42 +1000

    added ResetPasswordRequestedHandlerTest.php

---
+++ api/tests/Functional/Auth/ResetPassword/ResetPasswordRequestedHandlerTest.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Functional\Auth\ResetPassword;
+
+use App\Auth\Event\PasswordResetRequested;
+use App\Auth\MessageHandler\SendTokenOnResetPasswordHandler;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+final class ResetPasswordRequestedHandlerTest extends KernelTestCase
+{
+    public function testSuccess(): void
+    {
+        self::bootKernel();
+
+        $container = static::getContainer();
+        $handler = $container->get(SendTokenOnResetPasswordHandler::class);
+
+        $message = new PasswordResetRequested('user@test.ru', 'token-123');
+        $handler($message);
+
+        self::assertEmailCount(1);
+        self::assertEmailAddressContains(self::getMailerMessage(0), 'To', 'user@test.ru');
+        self::assertEmailHtmlBodyContains(self::getMailerMessage(0), 'token-123');
+    }
+}
