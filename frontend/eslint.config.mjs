e401b7d1e657fb0c813eaa82d852ae7da4379539
Flaaim <flaaim@list.ru>
2026-05-26 14:57:46 +1000

    installed eslint-plugin-tailwindcss

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,17 +1,28 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import tailwind from "eslint-plugin-tailwindcss";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  ...tailwind.configs["flat/recommended"],
+  {
+    settings: {
+      tailwindcss: {
+        config: {},
+        whitelist: ["bg-foreground", "text-background"],
+      },
+    },
+  },
   {
     rules: {
       "@typescript-eslint/no-unused-vars": "error",
       "react/style-prop-object": "error",
       "no-console": ["warn", { "allow": ["warn", "error"] }],
       "prefer-const": "error",
-      "semi": "error"
+      "semi": "error",
+      "tailwindcss/no-custom-classname": "warn",
     }
   },
   // Override default ignores of eslint-config-next.
