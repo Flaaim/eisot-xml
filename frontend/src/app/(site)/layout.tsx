d368fb69eb65353bae64aea8c0ab19ca17ee33c9
Flaaim <flaaim@list.ru>
2026-05-27 15:31:08 +1000

    refactored layout

---
+++ frontend/src/app/(site)/layout.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import {Metadata} from "next";
+
+export const metadata: Metadata = {
+  title: "Тесты ростехнадзора",
+  description: "Описание страницы",
+};
+
+export default function SiteLayout({children}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="grid min-h-screen grid-cols-[1fr_270px_700px_1fr] grid-rows-[auto_1fr_auto] gap-x-10 gap-y-12 max-[765px]:grid-cols-1 max-[765px]:grid-rows-[auto_auto_auto] max-[765px]:p-2.5">
+      <header className="col-start-2 col-end-4 max-[765px]:hidden">
+        Header
+      </header>
+      <main className="col-start-2 col-end-4">
+        {children}
+      </main>
+      <footer className="col-start-2 col-end-4">
+        Footer
+      </footer>
+    </div>
+  )
+}
--- frontend/src/app/(site)/page.tsx
+++ frontend/src/app/(site)/page.tsx
@@ -1,15 +1,7 @@
-import type {Metadata} from "next";
-
-export const metadata: Metadata = {
-  title: "Тесты ростехнадзора",
-  description: "Описание страницы",
-};
 
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-extrabold text-gray-600 sm:text-5xl">Привет!</h1>
-    </div>
+        <h1 className="text-4xl font-extrabold text-gray-600 sm:text-5xl">Привет!</h1>
   );
 }
