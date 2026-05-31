0b7033d46ec55b444a4f027b18a0adfa0066d59d
Flaaim <flaaim@list.ru>
2026-05-31 10:05:01 +1000

    added RefreshSessionAction

--- frontend/src/actions/auth.tsx
+++ frontend/src/actions/auth.tsx
@@ -48,9 +48,9 @@ export async function LoginAction(data: LoginData){
   const searchParams = new URLSearchParams({
     username: data.email,
     password: data.password,
-    grant_type: process.env.GRANT_TYPE,
-    client_id: process.env.CLIENT_ID,
-    client_secret: process.env.CLIENT_SECRET
+    grant_type: "password",
+    client_id: String(process.env.CLIENT_ID),
+    client_secret: String(process.env.CLIENT_SECRET)
   });
 
   try{
@@ -92,10 +92,81 @@ export async function LoginAction(data: LoginData){
         secure: process.env.NODE_ENV === 'production',
         maxAge: Number(result.expires_in),
       })
+      cookieStore.set({
+        name: 'refresh_token',
+        value: String(result.refresh_token),
+        httpOnly: true,
+        path: '/',
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: 2592000
+      })
+
     }
     return { success: true };
   }catch (error) {
     console.error("Fetch error:", error);
     return { success: false, error: "Не удалось подключиться к серверу API" };
+  }
+}
+
+export async function RefreshSessionAction() {
+  const cookieStore = await cookies();
+  const refreshToken = cookieStore.get('refresh_token');
+
+  if(!refreshToken){
+    return { success: false }
+  }
+
+  try {
+    const response = await fetch(`${process.env.INTERNAL_BACKEND_URL}/token`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },
+      body: JSON.stringify({
+        refresh_token: refreshToken.value,
+        grant_type: "refresh_token",
+        client_id: String(process.env.CLIENT_ID),
+        client_secret: String(process.env.CLIENT_SECRET)
+      })
+    })
+    const responseText = await response.text();
+
+    let result;
+    try {
+      result = responseText ? JSON.parse(responseText) : {};
+    } catch (parseError) {
+      console.error("Ошибка парсинга:", responseText);
+      return { success: false, error: "Сервер вернул некорректный ответ." };
+    }
+
+    if(!response.ok){
+      cookieStore.delete('refresh_token');
+      cookieStore.delete('access_token');
+      return { success: false }
+    }
+    if(response.ok && result.access_token){
+      cookieStore.set({
+        name: 'access_token',
+        value: String(result.access_token),
+        httpOnly: true,
+        path: '/',
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: Number(result.expires_in),
+      })
+      cookieStore.set({
+        name: 'refresh_token',
+        value: String(result.refresh_token),
+        httpOnly: true,
+        path: '/',
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: 2592000
+      })
+    }
+    return { success: true };
+  } catch (error) {
+    console.error("Fetch error:", error);
+    return {success: false, error: "Не удалось подключиться к серверу API"};
   }
 }
