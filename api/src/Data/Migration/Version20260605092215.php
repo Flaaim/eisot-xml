cb077d4a29008058f5d6b61c9c1b93727ab416d1
Flaaim <flaaim@list.ru>
2026-06-05 19:23:23 +1000

    added company migrations

---
+++ api/src/Data/Migration/Version20260605092215.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Data\Migration;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260605092215 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE companies (id VARCHAR NOT NULL, name VARCHAR NOT NULL, inn VARCHAR NOT NULL, PRIMARY KEY (id))');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE companies');
+    }
+}
